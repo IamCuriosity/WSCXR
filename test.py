beea156097ac727d362f35f46d48e62c0bfe98c3
nihaoqi <1946222053@qq.com>
2023-11-15 22:53:01 +0800

    init

---
+++ test.py
@@ -0,0 +1,249 @@
+import contextlib
+import logging
+import os
+import sys
+from wscxr.utils import create_logger
+import numpy as np
+import torch
+
+import wscxr.backbones
+import wscxr.common
+import wscxr.metrics
+import wscxr.patchcore
+import wscxr.sampler
+import wscxr.utils
+from wscxr.wscxr import SimpleNet
+from datetime import datetime
+from datasets.dataset import MedDataset,MedAbnormalDataset
+import tqdm
+import warnings
+import PIL
+from pytorch_grad_cam.utils.image import show_cam_on_image
+import torch.nn.functional as F
+import argparse
+from easydict import EasyDict
+import yaml
+
+
+warnings.filterwarnings('ignore')
+
+
+def normalization(segmentations,avgpool_size = 64):
+
+    segmentations_ = torch.tensor(segmentations[:, None, ...]).cuda()  # N x 1 x H x W
+    segmentations_ =  F.avg_pool2d(segmentations_, (avgpool_size,avgpool_size), stride=1).cpu().numpy()
+
+    min_scores = (
+        segmentations_.reshape(-1).min(axis=-1).reshape(1)
+    )
+
+    max_scores = (
+        segmentations_.reshape(-1).max(axis=-1).reshape(1)
+    )
+
+    segmentations = (segmentations - min_scores) / (max_scores - min_scores)
+    segmentations=np.clip(segmentations,a_min=0,a_max=1)
+
+    return segmentations
+
+
+def main(args):
+
+    dataloader_dict =dataset(
+                                os.path.join(args.config.data_path,args.dataset_name),
+                                args.config.batch_size,
+                                args.config.imagesize,
+                                args.config.num_workers,
+                                args.config.max_normal,
+                                )(args.seed)
+
+    device = wscxr.utils.set_torch_device(args.gpu)
+
+    imagesize = dataloader_dict["test"].dataset.imagesize
+
+    device_context = (
+        torch.cuda.device("cuda:{}".format(device.index))
+        if "cuda" in device.type.lower()
+        else contextlib.suppress()
+    )
+
+    wscxr.utils.fix_seeds(args.seed, device)
+
+    with device_context:
+
+        SimpleNet = create_samplenet_instance(
+            args.config.backbone_name,
+            args.config.layers_to_extract_from,
+            args.config.pretrain_embed_dimension,
+            args.config.target_embed_dimension,
+            args.config.patchsize,
+            args.config.meta_epochs,
+            args.config.gan_epochs,
+            args.config.dsc_layers,
+            args.config.dsc_hidden,
+            args.config.dsc_margin,
+            args.config.dsc_lr,
+
+        )(imagesize, device)
+
+        discriminator_path=os.path.join(args.config.results_path,args.dataset_name,args.config.dsc_save_path)
+        print("load discriminator path: {}".format(discriminator_path))
+        SimpleNet.discriminator.load_state_dict(torch.load(discriminator_path))
+
+        if SimpleNet.backbone.seed is not None:
+            wscxr.utils.fix_seeds(SimpleNet.backbone.seed, device)
+
+        torch.cuda.empty_cache()
+
+        scores, segmentations, labels_gt, image_paths = SimpleNet.predict(dataloader_dict["test"])
+
+        scores = np.array(scores)
+        scores = (scores - np.min(scores)) / (np.max(scores) - np.min(scores)).tolist()
+
+
+        results = wscxr.metrics.compute_imagewise_retrieval_metrics(scores, labels_gt)
+        print("auroc: {}, acc: {}, f1: {}".format(results['auroc'], results['acc'], results['f1']))
+
+        segmentations = normalization(np.array(segmentations))
+
+        save_images_root=os.path.join(args.config.results_path,args.dataset_name,"seg_images")
+        os.makedirs(save_images_root,exist_ok=True)
+
+        for image_path, segmentation ,label in tqdm.tqdm(
+                zip(image_paths,  segmentations,labels_gt),
+                total=len(image_paths),
+                desc="Generating Segmentation Images...",
+                leave=False,
+        ):
+            _, image_name = os.path.split(image_path)
+
+            image = PIL.Image.open(image_path).convert("RGB")
+            image = image.resize((args.config.imagesize,args.config.imagesize))
+            image = np.array(image).astype(np.uint8)
+
+            heat = show_cam_on_image(image / 255, segmentation, use_rgb=True)
+
+            label_= "normal" if label==0 else "abnormal"
+
+            PIL.Image.fromarray(heat).save(os.path.join(save_images_root,label_+"_"+image_name))
+
+
+
+def create_sampler(name, percentage):
+    def get_sampler(device):
+        if name == "identity":
+            return wscxr.sampler.IdentitySampler()
+        elif name == "greedy_coreset":
+            return wscxr.sampler.GreedyCoresetSampler(percentage, device)
+        elif name == "approx_greedy_coreset":
+            return wscxr.sampler.ApproximateGreedyCoresetSampler(percentage, device)
+
+    return  get_sampler
+
+
+def dataset(
+    data_path,
+    batch_size,
+    imagesize,
+    num_workers,
+    max_normal,
+):
+
+    def get_dataloaders(seed):
+
+
+        test_dataset = MedDataset(
+            data_path,
+            imagesize=imagesize,
+            split='test',
+            normal_only=False,
+            seed=seed,
+        )
+
+        print("test:{}".format(len(test_dataset)))
+
+        test_dataloader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+        test_dataloader.name = '{}_test'.format(args.dataset_name)
+
+        dataloader_dict = {
+            "test": test_dataloader
+        }
+
+        return dataloader_dict
+
+    return get_dataloaders
+
+
+def create_samplenet_instance(
+    backbone_name,
+    layers_to_extract_from,
+
+    pretrain_embed_dimension,
+    target_embed_dimension,
+
+    patchsize,
+    meta_epochs,
+    gan_epochs,
+
+    dsc_layers,
+    dsc_hidden,
+    dsc_margin,
+    dsc_lr,
+
+):
+
+    def get_simplenet(input_shape, device):
+        backbone_seed = None
+        backbone = wscxr.backbones.load(backbone_name)
+        backbone.name, backbone.seed = backbone_name, backbone_seed
+
+        simplenet_inst = SimpleNet(device)
+
+        simplenet_inst.load(
+                backbone=backbone,
+                layers_to_extract_from=layers_to_extract_from,
+                device=device,
+                input_shape=input_shape,
+                pretrain_embed_dimension=pretrain_embed_dimension,
+                target_embed_dimension=target_embed_dimension,
+
+                patchsize=patchsize,
+                meta_epochs=meta_epochs,
+                gan_epochs=gan_epochs,
+
+                dsc_layers=dsc_layers,
+                dsc_hidden=dsc_hidden,
+                dsc_margin=dsc_margin,
+                dsc_lr=dsc_lr,
+            )
+
+        return simplenet_inst
+
+    return get_simplenet
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="WSCXR")
+
+    parser.add_argument("--gpu", type=int, default=[0])
+    parser.add_argument("--seed", type=int, default=0)
+
+    parser.add_argument("--dataset_name", default='zhanglab', type=str,
+                        choices=['zhanglab', 'chexpert5', 'chexpert12'])
+
+    parser.add_argument("--faiss_on_gpu", type=bool, default=False)
+    parser.add_argument("--faiss_num_workers", type=int, default=8)
+
+    args = parser.parse_args()
+
+    with open(os.path.join("config", "{}.yaml".format(args.dataset_name))) as f:
+        args.config = EasyDict(yaml.load(f, Loader=yaml.FullLoader))
+
+    main(args)
---
+++ train.py
@@ -0,0 +1,338 @@
+import argparse
+import contextlib
+import os
+import pprint
+import warnings
+from datetime import datetime
+
+import torch
+import yaml
+from easydict import EasyDict
+
+import wscxr.backbones
+import wscxr.common
+import wscxr.metrics
+import wscxr.patchcore
+import wscxr.sampler
+import wscxr.utils
+from datasets.dataset import MedDataset, MedAbnormalDataset
+from wscxr.wscxr import SimpleNet
+from wscxr.utils import create_logger
+
+warnings.filterwarnings('ignore')
+
+
+def get_current_time():
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    return current_time
+
+
+def main(args):
+
+    run_save_path = os.path.join(args.config.results_path,args.dataset_name)
+    os.makedirs(run_save_path,exist_ok=True)
+
+    current_time = get_current_time()
+
+    logger = wscxr.utils.create_logger("logger", os.path.join(run_save_path, "log_{}.log".format(current_time)))
+
+    logger.info("args: {}".format(pprint.pformat(args)))
+
+    dataloader_dict =dataset(   os.path.join(args.config.data_path,args.dataset_name),
+                                args.config.batch_size,
+                                args.config.imagesize,
+                                args.config.num_workers,
+                                args.config.max_normal,
+                                )(args.seed)
+
+    device = wscxr.utils.set_torch_device(args.gpu)
+
+    imagesize = dataloader_dict["train_normal"].dataset.imagesize
+
+    device_context = (
+        torch.cuda.device("cuda:{}".format(device.index))
+        if "cuda" in device.type.lower()
+        else contextlib.suppress()
+    )
+
+    wscxr.utils.fix_seeds(args.seed, device)
+
+    with device_context:
+
+        torch.cuda.empty_cache()
+        sampler = create_sampler('approx_greedy_coreset',args.config.normal_percentage)(
+            device,
+        )
+
+        PatchCore = create_patchcore_instance(
+                     args.config.backbone_name,
+                     args.config.layers_to_extract_from,
+                     args.config.pretrain_embed_dimension,
+                     args.config.target_embed_dimension,
+                     args.config.preprocessing,
+                     args.config.aggregation,
+                     args.config.patchsize,
+                     args.config.patchscore,
+                     args.config.patchoverlap,
+                     args.config.anomaly_scorer_num_nn,
+                     args.config.patchsize_aggregate,
+                     args.faiss_on_gpu,
+                     args.faiss_num_workers)(imagesize, sampler, device)
+
+        if PatchCore.backbone.seed is not None:
+            wscxr.utils.fix_seeds(PatchCore.backbone.seed, device)
+
+        torch.cuda.empty_cache()
+
+        normal_features = PatchCore.fit(dataloader_dict["train_normal"])
+        PatchCore.featuresampler = wscxr.sampler.IdentitySampler()
+        abnormal_features = PatchCore.fit(dataloader_dict["train_abnormal"])
+
+        PatchCore.anomaly_scorer.fit([normal_features])
+        anomaly_scores = PatchCore.anomaly_scorer.predict([abnormal_features])[0]
+
+        _,index=torch.topk(torch.from_numpy(anomaly_scores),
+                           int(anomaly_scores.shape[0]*args.config.abnormal_percentage),
+                           largest=True)
+
+
+        abnormal_features=abnormal_features[index.numpy()]
+
+        logger.info("abnormal_prototype shape:({},{}), normal_prototype shape:({},{})".
+                    format(abnormal_features.shape[0],abnormal_features.shape[1],
+                           normal_features.shape[0],normal_features.shape[1],))
+
+
+        SimpleNet = create_samplenet_instance(
+            args.config.backbone_name,
+            args.config.layers_to_extract_from,
+
+            args.config.pretrain_embed_dimension,
+            args.config.target_embed_dimension,
+            args.config.patchsize,
+            args.config.meta_epochs,
+            args.config.gan_epochs,
+            args.config.dsc_layers,
+            args.config.dsc_hidden,
+            args.config.dsc_margin,
+            args.config.dsc_lr,
+            abnormal_features,
+            logger,
+            os.path.join(run_save_path ,args.config.dsc_save_path),
+        )(imagesize, device)
+
+        if SimpleNet.backbone.seed is not None:
+            wscxr.utils.fix_seeds(SimpleNet.backbone.seed, device)
+
+        torch.cuda.empty_cache()
+
+        i_auroc, p_auroc, pro_auroc = SimpleNet.train_(dataloader_dict["train_normal"], dataloader_dict["test"])
+        logger.info("best i_auroc: {}, best i_acc: {}, i_f1: {}".format(i_auroc, p_auroc, pro_auroc))
+
+
+def create_sampler(name, percentage):
+    def get_sampler(device):
+        if name == "identity":
+            return wscxr.sampler.IdentitySampler()
+        elif name == "greedy_coreset":
+            return wscxr.sampler.GreedyCoresetSampler(percentage, device)
+        elif name == "approx_greedy_coreset":
+            return wscxr.sampler.ApproximateGreedyCoresetSampler(percentage, device)
+
+    return  get_sampler
+
+
+
+def dataset(
+    data_path,
+    batch_size,
+    imagesize,
+    num_workers,
+    max_normal,
+):
+
+    def get_dataloaders(seed):
+
+        train_normal_dataset = MedDataset(
+            data_path,
+            imagesize=imagesize,
+            split='train',
+            seed=seed,
+            normal_only=True,
+            max_normal=max_normal,
+        )
+
+        train_abnormal_dataset = MedAbnormalDataset(
+            data_path,
+            args.config.abnormal_k,
+            imagesize=imagesize,
+            split='train',
+            seed=seed,
+        )
+
+        test_dataset = MedDataset(
+            data_path,
+            imagesize=imagesize,
+            split='test',
+            normal_only=False,
+            seed=seed,
+        )
+        print("train_normal:{}, train_abnormal:{}, test:{}".
+              format(len(train_normal_dataset),len(train_abnormal_dataset),len(test_dataset)))
+
+        train_normal_dataloader = torch.utils.data.DataLoader(
+            train_normal_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+        train_abnormal_dataloader = torch.utils.data.DataLoader(
+            train_abnormal_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+        test_dataloader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+        train_normal_dataloader.name = '{}_train_normal'.format(args.dataset_name)
+        train_abnormal_dataloader.name = '{}_train_abnormal'.format(args.dataset_name)
+        test_dataloader.name = '{}_test'.format(args.dataset_name)
+
+        dataloader_dict = {
+            "train_normal": train_normal_dataloader,
+            "train_abnormal": train_abnormal_dataloader,
+            "test": test_dataloader
+        }
+
+        return dataloader_dict
+
+    return get_dataloaders
+
+
+
+def create_patchcore_instance(
+    backbone_name,
+    layers_to_extract_from,
+    pretrain_embed_dimension,
+    target_embed_dimension,
+    preprocessing,
+    aggregation,
+    patchsize,
+    patchscore,
+    patchoverlap,
+    anomaly_scorer_num_nn,
+    patchsize_aggregate,
+    faiss_on_gpu,
+    faiss_num_workers,
+):
+
+    def get_patchcore(input_shape, sampler, device):
+        backbone_seed = None
+
+        backbone = wscxr.backbones.load(backbone_name)
+        backbone.name, backbone.seed = backbone_name, backbone_seed
+
+        nn_method = wscxr.common.FaissNN(faiss_on_gpu, faiss_num_workers)
+
+        patchcore_instance = wscxr.patchcore.PatchCore(device)
+        patchcore_instance.load(
+                backbone=backbone,
+                layers_to_extract_from=layers_to_extract_from,
+                device=device,
+                input_shape=input_shape,
+                pretrain_embed_dimension=pretrain_embed_dimension,
+                target_embed_dimension=target_embed_dimension,
+                patchsize=patchsize,
+                featuresampler=sampler,
+                anomaly_scorer_num_nn=anomaly_scorer_num_nn,
+                nn_method=nn_method,
+            )
+
+        return patchcore_instance
+
+    return  get_patchcore
+
+
+def create_samplenet_instance(
+    backbone_name,
+    layers_to_extract_from,
+
+    pretrain_embed_dimension,
+    target_embed_dimension,
+
+    patchsize,
+    meta_epochs,
+    gan_epochs,
+
+    dsc_layers,
+    dsc_hidden,
+    dsc_margin,
+    dsc_lr,
+
+    prototypes,
+    logger,
+
+    dsc_save_path,
+):
+
+    def get_simplenet(input_shape, device):
+        backbone_seed = None
+        backbone = wscxr.backbones.load(backbone_name)
+        backbone.name, backbone.seed = backbone_name, backbone_seed
+
+        simplenet_inst = SimpleNet(device)
+
+        simplenet_inst.load(
+                backbone=backbone,
+                layers_to_extract_from=layers_to_extract_from,
+                device=device,
+                input_shape=input_shape,
+                pretrain_embed_dimension=pretrain_embed_dimension,
+                target_embed_dimension=target_embed_dimension,
+
+                logger=logger,
+                dsc_save_path=dsc_save_path,
+                prototypes=prototypes,
+
+                patchsize=patchsize,
+                meta_epochs=meta_epochs,
+                gan_epochs=gan_epochs,
+
+                dsc_layers=dsc_layers,
+                dsc_hidden=dsc_hidden,
+                dsc_margin=dsc_margin,
+                dsc_lr=dsc_lr,
+
+            )
+        return simplenet_inst
+
+    return get_simplenet
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="WSCXR")
+
+    parser.add_argument("--gpu", type=int, default=[0])
+    parser.add_argument("--seed", type=int, default=0)
+
+    parser.add_argument("--dataset_name", default='zhanglab', type=str,choices=['zhanglab','chexpert5','chexpert12'])
+
+    parser.add_argument("--faiss_on_gpu", type=bool, default=False)
+    parser.add_argument("--faiss_num_workers", type=int, default=8)
+
+    args = parser.parse_args()
+
+    with open(os.path.join("config", "{}.yaml".format(args.dataset_name))) as f:
+        args.config = EasyDict(yaml.load(f, Loader=yaml.FullLoader))
+
+    main(args)
